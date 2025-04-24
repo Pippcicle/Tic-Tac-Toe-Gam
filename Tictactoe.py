1000dc6720fece67eceab91198f32f94be89d207
Pippcicle <pippaamsmith@gmail.com>
2025-04-24 18:53:44 +0100

    Add files via upload

--- Tictactoe.py
+++ Tictactoe.py
@@ -25,4 +25,37 @@ def winner(b,l):
            (b[0][2] == l and b[1][1] == l and b[2][0] == l)
            )
 
+#configure text on button while playing with another player
+def get_text(i, j, l1, l2, gb): 
+    global sign, board
+    if board[i][j] == " ":
+        if sign % 2 == 0 : 
+            l1.config(state = DISABLED)
+            l2.config(state = ACTIVE)
+            board[i][j] = "X"
+        else : 
+            l1.config(state = ACTIVE)
+            l2.config(state = DISABLED)
+            board[i][j] = "O"
+        sign += 1 
+        button[i][j].config(text = board[i][j])
+    if winner(board,"X"): 
+        gb.destroy()
+        box = messagebox.showinfo("Winner","Player 1 is the winner")
+    elif winner(board, "O"):
+        gb.destroy()
+        box = messagebox.showinfo("Winner", "Player 2 is the winner")
+    else : 
+        gb.destroy()
+        box = messagebox.showinfo("Tie", "The game is a tie")
+
+def isfull():
+    flag = True
+    for i in board : 
+        if (i.count(" ") > 0 ): 
+            flag = False
+    return flag
+
+
+
 
